be18ec3896ae8cb33e1ddb0d047921e31ff86323
JoaoDognini <82457881+JoaoDognini@users.noreply.github.com>
2022-01-21 11:37:49 -0300

    Implementando Service de Gerenciamento de Moedas

--- conversor-de-moedas/src/app/conversor/conversor.module.ts
+++ conversor-de-moedas/src/app/conversor/conversor.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ConversorComponent } from './components';
-import { MoedaService } from '.';
+import { MoedaService } from './services';
 
 
 
